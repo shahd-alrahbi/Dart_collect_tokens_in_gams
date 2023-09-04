15bc31427671e46d39ba287c29c994acc21d85c5
shahd-alrahbi <138566899+shahd-alrahbi@users.noreply.github.com>
2023-09-04 20:19:52 +0400

    Initial commit

---
+++ bin/collect_tokens_in_gams.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  // Adam, Bella, and Carl collect tokens in the game, Bella has 10 more tokens than Adam,
+  // and Carl has 8 more tokens than Bella, Altogether they have 91 tokens, how many tokens does Adam have?
+  
+  double multadam = 3;
+  double result = 63;
+
+  double adam = result / multadam;
+
+  print('Adam = $adam');
+}
---
+++ test/collect_tokens_in_gams_test.dart
@@ -0,0 +1,8 @@
+import 'package:collect_tokens_in_gams/collect_tokens_in_gams.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
